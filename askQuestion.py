ba211f7615a687fc575357ca1ae71c5599897e15
Amanda Nystedt <am4421ny-s@student.lu.se>
2025-02-20 15:32:21 +0100

    More fixes

--- askQuestion.py
+++ askQuestion.py
@@ -61,7 +61,6 @@ def checkRelevance(chunk, user_input, sources):
     return "", None
 
 def generate_response(context, user_input):
-<<<<<<< HEAD
     system_prompt = (
         "You are an assistant for question-answering tasks. "
         "Use the following pieces of retrieved context to answer the question. "
@@ -74,15 +73,6 @@ def generate_response(context, user_input):
         f"Only answer the question based on this information: {user_input}"
     )
     return utils.send_gpt4o_prompt(system_prompt, user_prompt, 200)
-=======
-    response = openai.chat.completions.create(
-        model="o3-mini",
-        messages=[
-            {"role": "system", "content": "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. You should answer conscisely and not add extra information that is not avaliable in the context. If you do not know the answer, say that you don't have information on that topic."},
-            {"role": "user", "content": f"Here are the chunks extracted from the document that are relevant to the question: {user_input}. Please provide a an of the relevant information. Only answer the question based on this information: {context}"}
-        ]    )
-    return response.choices[0].message.content
->>>>>>> 767f2146d062de66fa6bb5d9480598ebbe1e9cd8
 
 def filter_chunks(chunks, user_input):
     context = ""
